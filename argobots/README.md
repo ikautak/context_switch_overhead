f8b2fcbf0a1bcdc59b320580ddbe040a7ae9c9f1
Katsuaki Oshio <ikautak@gmail.com>
2022-08-11 11:26:43 +0900

    feat: add argobots ver

---
+++ argobots/README.md
@@ -0,0 +1,13 @@
+# argobots setup
+
+see https://github.com/pmodels/argobots#1-getting-started
+
+```
+sudo apt install autoconf libtool
+cd argobots
+./autogen.sh
+./configure --prefix=`pwd`/../argobots-install
+make
+make install
+```
+
---
+++ argobots/main.cc
@@ -0,0 +1,45 @@
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+#include "abt.h"
+
+constexpr int THREAD_NUM = 100;
+constexpr int YIELD_COUNT = 100'000;
+
+void func(void* arg) {
+  volatile int i = 0;
+
+  while (i < YIELD_COUNT) {
+    ++i;
+
+    ABT_thread_yield();
+  }
+}
+
+int main(int argc, char* argv[]) {
+  cpu_set_t cpu_set;
+  CPU_ZERO(&cpu_set);
+  CPU_SET(0, &cpu_set);
+  if (sched_setaffinity(0, sizeof(cpu_set_t), &cpu_set)) {
+    std::cerr << "setaffinity error" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  ABT_xstream xstream;
+  ABT_pool pool;
+  std::vector<ABT_thread> threads(THREAD_NUM);
+
+  ABT_init(argc, argv);
+  ABT_xstream_self(&xstream);
+  ABT_xstream_create(ABT_SCHED_NULL, &xstream);
+  ABT_xstream_get_main_pools(xstream, 1, &pool);
+
+  for (int i = 0; i < THREAD_NUM; ++i) {
+    ABT_thread_create(pool, func, nullptr, ABT_THREAD_ATTR_NULL, &threads[i]);
+  }
+
+  ABT_thread_join_many(THREAD_NUM, &threads[0]);
+
+  return EXIT_SUCCESS;
+}
